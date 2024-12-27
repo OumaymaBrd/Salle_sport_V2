d968032f8bd85da3b455d78b9326675fc2773041
OumaymaBrd <oumaymalovemaths>
2024-12-27 22:50:00 +0100

    cacher

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_destroy();
-setcookie("user_logged", "", time() - 3600, "/");
-setcookie("matricule", "", time() - 3600, "/");
-header("Location: login.php");
-exit();
-
-?>
